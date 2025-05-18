c1cfe54f4262b10fe53195e4697a7722bd47501b
l4rralde <ealarralde@gmail.com>
2025-05-17 18:46:06 -0600

    Training autoencoder for twits

---
+++ train_twits_autoencoder.py
@@ -0,0 +1,33 @@
+import torch
+from torch.utils.data import DataLoader
+
+from src.dataset import load_twits_dataset
+from src.autoencoder import Autoencoder, BowDataset, train
+
+
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+
+
+def main() -> None:
+    X, y = load_twits_dataset()
+    dataset = BowDataset(X, y)
+    dataloader = DataLoader(dataset, batch_size=256)
+    M, N = X.shape
+    autoencoder = Autoencoder(N, 2, n_classes=2, tag='twits')
+    autoencoder = autoencoder.to(device)
+
+    hist = train(
+        autoencoder,
+        500,
+        device,
+        dataloader,
+        alpha=1.0
+    )
+
+if __name__ == '__main__':
+    main()
