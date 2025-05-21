65aa38668dabda0ed075883face150aff3e7368c
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2025-05-20 21:03:33 -0600

    Some fixes

--- src/misc.py
+++ src/misc.py
@@ -2,21 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_2D_project(project: np.ndarray, title: str, y: np.ndarray, labels: dict) -> None:
-    plt.figure(figsize=(6, 4))
-    for i, label in labels.items():
-        idx = y == i
-        plt.scatter(*project[idx].T, label=label, alpha=0.3)
-    plt.legend()
-    plt.title(title)
-    plt.show()
-
-
-def plot_3D_project(project: np.ndarray, title: str, y: np.ndarray, labels: dict) -> None:
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+def plot_2D_project(project: np.ndarray, title: str, y: np.ndarray, labels: dict, ax: object = None) -> None:
+    if ax is None:
+        fig = plt.figure(figsize=(6, 4))
+        ax = fig.add_subplot()
     for i, label in labels.items():
         idx = y == i
         ax.scatter(*project[idx].T, label=label, alpha=0.3)
     ax.legend()
+    ax.set_title(title)
+
+
+def plot_3D_project(project: np.ndarray, title: str, y: np.ndarray, labels: dict, ax: object=None) -> None:
+    if ax is None:
+        fig = plt.figure(figsize=(6, 4))
+        ax = fig.add_subplot(projection='3d')
+    for i, label in labels.items():
+        idx = y == i
+        ax.scatter(*project[idx].T, label=label, alpha=0.3)
+    ax.legend()
+    ax.set_title(title)
+
+
+def axplot_project(
+        project: np.ndarray,
+        title: str,
+        y: np.ndarray,
+        labels: dict,
+        ax: object,
+        legend: bool = False
+    ) -> None:
+    for i, label in labels.items():
+        idx = y == i
+        ax.scatter(*project[idx].T, label=label, alpha=0.3)
+    if legend:
+        ax.legend()
     ax.set_title(title)
